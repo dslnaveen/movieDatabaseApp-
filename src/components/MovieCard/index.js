b33473cf0506edf63e1eba18f7412c145727ec55
dslnaveen <dslanveen178@gmail.com>
2024-12-05 05:17:30 +0000

    solution

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,19 +1,24 @@
-import './index.css'
+
 import {Link} from 'react-router-dom'
+import './index.css'
+
 const MovieCard = props => {
   const {movieDetails} = props
-  const {id, title, posterPath, voteAverage, originalLanguage} = movieDetails
-  const imgUrl = `https://image.tmdb.org/t/p/w500/${posterPath}`
+  const {id, title, posterPath, voteAverage} = movieDetails
+
   return (
-    <Link to={`/movie/${id}`} className="movie-link">
-      <li className="movie-card">
-        <img className="img" src={imgUrl} alt={id} />
-        <div className="wrapper-div">
-          <h1 className="movie-name">{title}</h1>
-          <p className="rating">{`${voteAverage.toFixed(1)}/10`}</p>
-        </div>
-      </li>
-    </Link>
+    <li className="movie-card-container col-12 col-sm-6 col-lg-2 mb-3 d-flex flex-column">
+      <img className="movie-card-image" alt={title} src={posterPath} />
+      <div className="d-flex flex-column align-items-center mt-2">
+        <h1 className="movie-title m-0">{title}</h1>
+        <p className="movie-rating mb-0 ms-1">Rating: {voteAverage}</p>
+      </div>
+      <Link to={`/movie/${id}`} className="mt-auto align-self-center">
+        <button className="btn btn-outline-success" type="button">
+          View Details
+        </button>
+      </Link>
+    </li>
   )
 }
 
